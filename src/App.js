abddd3e20fd34922c778217d1f0a977493e31f15
Ricardo Souza <rr_souza@outlook.com>
2023-05-25 21:50:14 -0300

    Criado temas e classes iniciais

--- src/App.js
+++ src/App.js
@@ -1,22 +1,25 @@
-import logo from './logo.svg';
 import './App.scss';
+import DarkModeIcon from '@mui/icons-material/DarkMode';
+import LightModeIcon from '@mui/icons-material/LightMode';
 
 function App() {
+  let isDarkMode = false;
+
+  const setTheme = (newValue) => {
+    console.log('isDarkMode antes: ', isDarkMode);
+    isDarkMode = newValue;
+    console.log('isDarkMode depois: ', isDarkMode);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className={ `d-flex ${isDarkMode ? 'dark-theme' : 'light-theme'}` }>
+      <header className="app-header bg-primary-800 d-flex justify-content-between">
+        <span className="text-light hover cursor-pointer">
+          <DarkModeIcon className="theme-icon" onClick={ () => setTheme(true) } />
+        </span>
+        <span className="text-light hover cursor-pointer">
+          <LightModeIcon className="theme-icon" onClick={ () => setTheme(false) } />
+        </span>
       </header>
     </div>
   );
