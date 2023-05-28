b050fdaa63ddaba1e0fc24ee71b031a1cf48fa76
Ricardo Souza <rr_souza@outlook.com>
2023-05-28 14:42:13 -0300

    Configuração do palette do Material UI

--- src/App.js
+++ src/App.js
@@ -2,10 +2,27 @@ import React from 'react';
 
 import './App.scss';
 import HeaderNavbar from './components/HeaderNavbar/HeaderNavbar';
+import { createTheme } from '@mui/material';
+import { ThemeProvider } from '@emotion/react';
 
 function App() {
+  const theme = createTheme({
+    palette: {
+      mode: 'light',
+      primary: {
+        main: '#00816D',
+        dark: '#245652',
+      },
+      secondary: {
+        main: '#e64a19',
+      },
+    },
+  });
+
   return (
-    <HeaderNavbar />
+    <ThemeProvider theme={theme}>
+      <HeaderNavbar showThemeIcons="true" />
+    </ThemeProvider>
   );
 }
 
--- src/components/HeaderNavbar/HeaderNavbar.js
+++ src/components/HeaderNavbar/HeaderNavbar.js
@@ -1,31 +1,27 @@
-import React, {useState} from 'react';
+import React from 'react';
 
 import './HeaderNavbar.scss';
-import DarkModeIcon from '@mui/icons-material/DarkMode';
-import LightModeIcon from '@mui/icons-material/LightMode';
+import { Button, Stack } from '@mui/material';
 
 function HeaderNavbar(props) {
-  const [currentTheme, setCurrentTheme] = useState('light-theme');
-
   return (
-    <div className={ `d-flex ${currentTheme} w-100` }>
-      <header className="app-header d-flex justify-content-between w-100 align-items-center pt-4">
-        <button className="btn btn-logo p-0">
-          <div className="product-logo"></div>
-        </button>
-
-        { props.showThemeIcons === 'true' ? (
-          <div className="d-flex themes">
-          <span className="cursor-pointer">
-            <LightModeIcon className="theme-icon light" onClick={ () => setCurrentTheme('light-theme') } />
-          </span>
-          <span className="cursor-pointer">
-            <DarkModeIcon className="theme-icon dark" onClick={ () => setCurrentTheme('dark-theme') } />
-          </span>
-        </div>
-        ) : '' }
-      </header>
-    </div>
+    <>
+      <div className={ `d-flex w-100` }>
+        <header className="app-header d-flex justify-content-between w-100 align-items-center pt-4">
+          <button className="btn btn-logo p-0">
+            <div className="product-logo"></div>
+          </button>
+        </header>
+      </div>
+      <div className="d-flex row">
+      <Stack spacing={2} direction="row">
+        <Button variant="text">Text</Button>
+        <Button variant="contained">Contained</Button>
+        <Button variant="contained" color='secondary'>Contained</Button>
+        <Button variant="outlined" color='secondary'>Outlined</Button>
+      </Stack>
+      </div>
+    </>
   )
 }
 
