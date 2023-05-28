267c740e0444ae9fbf3ba653c19935596ea12809
Ricardo Souza <rr_souza@outlook.com>
2023-05-28 13:17:09 -0300

    Finalizado configuração básica do header

--- src/App.js
+++ src/App.js
@@ -1,27 +1,11 @@
+import React from 'react';
+
 import './App.scss';
-import DarkModeIcon from '@mui/icons-material/DarkMode';
-import LightModeIcon from '@mui/icons-material/LightMode';
+import HeaderNavbar from './components/HeaderNavbar/HeaderNavbar';
 
 function App() {
-  let isDarkMode = false;
-
-  const setTheme = (newValue) => {
-    console.log('isDarkMode antes: ', isDarkMode);
-    isDarkMode = newValue;
-    console.log('isDarkMode depois: ', isDarkMode);
-  }
-
   return (
-    <div className={ `d-flex ${isDarkMode ? 'dark-theme' : 'light-theme'}` }>
-      <header className="app-header bg-primary-800 d-flex justify-content-between">
-        <span className="text-light hover cursor-pointer">
-          <DarkModeIcon className="theme-icon" onClick={ () => setTheme(true) } />
-        </span>
-        <span className="text-light hover cursor-pointer">
-          <LightModeIcon className="theme-icon" onClick={ () => setTheme(false) } />
-        </span>
-      </header>
-    </div>
+    <HeaderNavbar />
   );
 }
 
